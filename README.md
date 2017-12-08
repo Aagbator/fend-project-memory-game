3c6813dca7e92df09e0735055184cdad1e542c7c
aagbator <tonjoe99@gmail.com>
2017-12-08 04:13:57 -0500

    fixes based on instructor feedback

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# Memory Game Project
+# Memory Game Project by Anthony
 
-## Table of Contents
-
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## About the Game
+ This game requires s player to match similar cards. It consists of 16 cards; composed of 8 pairs, face down in random order.
+ You have to turn over two cards at a time, with the goal of turning over a matching pair.
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- /*
+/*
  * Create a list that holds all of your cards
  */
 
@@ -10,111 +10,134 @@
  *   - add each card's HTML to the page
  */
 
- var opened_card = [];
- var active_card = [];
- var temp_card = [];
- var selected_count = 0;
- var moves = 0;
- var status = true;
- var rating = 0;
- var sec = 0;
+var opened_card = [];
+var active_card = [];
+var temp_card = [];
+var selected_count = 0;
+var moves = 0;
+var status = true;
+var rating = 0;
+var sec = 0;
+var intervalId;
+var popup = $(".popup");
+let time_status = 0;
 
- var popup = $(".popup");
-
- var list = [
-     {id:1, card_id:100, matched: false, symbol: "fa-diamond"},
-     {id:2, card_id:100, matched: false, symbol: "fa-diamond"},
-     {id:3, card_id:200, matched: false, symbol: "fa-anchor"},
-     {id:4, card_id:200, matched: false, symbol: "fa-anchor"},
-     {id:5, card_id:300, matched: false, symbol: "fa-cube"},
-     {id:6, card_id:300, matched: false, symbol: "fa-cube"},
-     {id:7, card_id:400, matched: false, symbol: "fa-bolt"},
-     {id:8, card_id:400, matched: false, symbol: "fa-bolt"},
-     {id:9, card_id:500, matched: false, symbol: "fa-paper-plane-o"},
-     {id:10, card_id:500, matched: false, symbol: "fa-paper-plane-o"},
-     {id:11, card_id:600, matched: false, symbol: "fa-leaf"},
-     {id:12, card_id:600, matched: false, symbol: "fa-leaf"},
-     {id:13, card_id:700, matched: false, symbol: "fa-bicycle"},
-     {id:14, card_id:700, matched: false, symbol: "fa-bicycle"},
-     {id:15, card_id:800, matched: false, symbol: "fa-bomb"},
-     {id:16, card_id:800, matched: false, symbol: "fa-bomb"},
- ];
+var list = [
+    {id: 1, card_id: 100, matched: false, symbol: "fa-diamond"},
+    {id: 2, card_id: 100, matched: false, symbol: "fa-diamond"},
+    {id: 3, card_id: 200, matched: false, symbol: "fa-anchor"},
+    {id: 4, card_id: 200, matched: false, symbol: "fa-anchor"},
+    {id: 5, card_id: 300, matched: false, symbol: "fa-cube"},
+    {id: 6, card_id: 300, matched: false, symbol: "fa-cube"},
+    {id: 7, card_id: 400, matched: false, symbol: "fa-bolt"},
+    {id: 8, card_id: 400, matched: false, symbol: "fa-bolt"},
+    {id: 9, card_id: 500, matched: false, symbol: "fa-paper-plane-o"},
+    {id: 10, card_id: 500, matched: false, symbol: "fa-paper-plane-o"},
+    {id: 11, card_id: 600, matched: false, symbol: "fa-leaf"},
+    {id: 12, card_id: 600, matched: false, symbol: "fa-leaf"},
+    {id: 13, card_id: 700, matched: false, symbol: "fa-bicycle"},
+    {id: 14, card_id: 700, matched: false, symbol: "fa-bicycle"},
+    {id: 15, card_id: 800, matched: false, symbol: "fa-bomb"},
+    {id: 16, card_id: 800, matched: false, symbol: "fa-bomb"},
+];
 
 
- function new_game(){
-     opened_card = [];
-     active_card = [];
-     temp_card = [];
-     selected_count = 0;
-     moves = 0;
-     list = shuffle(list);
-     updatesStars(moves);
-     updateTimer(true);
+// new game function
+function new_game() {
+    $('.popup').removeClass('open-popup');
+    opened_card = [];
+    active_card = [];
+    temp_card = [];
+    selected_count = 0;
+    moves = 0;
+    list = shuffle(list);
+    updatesStars(moves);
+    //updateTimer(true,false);
 
-     $('.popup').hide();
-     $('.deck').empty();
-     $(".moves").text(0);
+
+    $('.deck').empty();
+    $(".moves").text(0);
 
     var cards = [];
-    list.forEach(function(ele){
+    list.forEach(function (ele) {
         var element = `<li class='card' data-id='${ele.id}' data-matched='false' data-card_id='${ele.card_id}'><i class='fa ${ele.symbol}'></i></li>`;
 
         $('.deck').append(element);
         cards.push(element);
     });
 
-     $('.deck').append(popup);
+    $('.deck').append(popup);
 
-     $('.card').on('click', function() {
-         if($(this).hasClass("match")){
-             return false;
-         }
-         selected_count++;
-         $(this).addClass("open show").removeClass("mismatched");
-         temp_card[0] = $(this).data("id");
-         temp_card[1] = $(this).data("card_id");
+    $('.card').on('click', function () {
+        time_status++;
 
-         active_card[0] = opened_card[0];
-         active_card[1] = opened_card[1];
-         opened_card[0] = temp_card[0];
-         opened_card[1] = temp_card[1];
+        if (time_status == 1) {// starts the timer when a card is clicked
+            startTimer();
+        }
+
+        //prevents the moves from updating when a matched card is clicked again
+        if ($(this).hasClass("match")) {
+            return false;
+        }
+
+        //prevents the moves from updating when an open cards is clicked again
+        if ($(this).hasClass("open show")) {
+            return false;
+        }
+
+        selected_count++;
+        $(this).addClass("open show").removeClass("mismatched");
+        temp_card[0] = $(this).data("id");
+        temp_card[1] = $(this).data("card_id");
+
+        active_card[0] = opened_card[0]; //pass the current opened card ID to the active element
+        active_card[1] = opened_card[1]; //pass the current opened card CARD ID to the active element
+        opened_card[0] = temp_card[0];
+        opened_card[1] = temp_card[1];
 
 
-         if((active_card[1] == opened_card[1])&&(active_card[0] !== opened_card[0])){
-             setCardAsMatched(active_card[0]);
-             setCardAsMatched(opened_card[0]);
-             selected_count = 0;
-             moves++;
-             checkIfAllMatched();
-         }
-         else{
-             if(selected_count == 2){
-                 unsetCardAsMatched(active_card[0]);
-                 unsetCardAsMatched(opened_card[0]);
-                 setTimeout(function(){
-                     $('.card').each(function() {
-                         $(this).removeClass("mismatched");
-                     });
-                     }, 500);
-                 selected_count = 0;
-                 moves++;
-             }
-         }
-         updateMoves(moves);
-         updatesStars(moves);
-     });
+        //check if the already opened card and the newly opened card match and the already opened card isnt clicked
+        if ((active_card[1] == opened_card[1]) && (active_card[0] !== opened_card[0])) {
+            setCardAsMatched(active_card[0]);
+            setCardAsMatched(opened_card[0]);
+            selected_count = 0;
+            moves++;
+            checkIfAllMatched();
+        }
+        else {
+            if (selected_count == 2) {
+                unsetCardAsMatched(active_card[0]);
+                unsetCardAsMatched(opened_card[0]);
+                setTimeout(function () {
+                    $('.card').each(function () {
+                        $(this).removeClass("mismatched");
+                    });
+                }, 300);
+                selected_count = 0;
+                moves++;
+            }
+        }
+        updateMoves(moves);
+        updatesStars(moves);
+    });
 }
 
- new_game();
+new_game();
 
 
-
+//restart game when "play again" is clicked
 $(".restart").on('click', function () {
     restart();
 });
 
+
+//restart game function
 function restart() {
-    $('.popup').hide();
+    $('.popup').removeClass('open-popup');
+    resetTimer();
+    time_status = 0;
+    $(".seconds").text(0);
+    //startTimer();
     new_game();
 }
 
@@ -134,7 +157,6 @@ function shuffle(array) {
 }
 
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -149,75 +171,104 @@ function shuffle(array) {
 
 
 
+//accept a single card and set it as matched
+function setCardAsMatched(element) {
+    console.log("element id = " + element);
+    $('.card').each(function () {
+        var picked = $(this).data("id");
+        if (picked == element) {
+            console.log("cards exist");
+            $(this).addClass("match").attr("data-matched", "true");
+        }
+    });
+}
 
- function setCardAsMatched(element){
-        console.log("element id = "+element);
-         $('.card').each(function() {
-             var picked = $(this).data("id");
-              if(picked == element){
-                  console.log("cards exist");
-                  $(this).addClass("match").attr("data-matched","true");
-              }
-         });
- }
+//accept a single card and unset it as matched
+function unsetCardAsMatched(element) {
+    $('.card').each(function () {
+        var picked = $(this).data("id");
+        if (picked == element) {
+            //console.log("cards unmatched");
+            $(this).removeClass("open show").addClass("mismatched");
+            //$(this).removeClass("red");
+        }
+    });
+}
 
- function unsetCardAsMatched(element){
-     $('.card').each(function() {
-         var picked = $(this).data("id");
-         if(picked == element){
-             //console.log("cards unmatched");
-             $(this).removeClass("open show").addClass("mismatched");
-             //$(this).removeClass("red");
-         }
-     });
- }
+//updates the total moves
+function updateMoves(moves) {
+    $(".moves").text(moves);
+}
 
- function updateMoves(moves){
-     $(".moves").text(moves);
- }
+//updates the starts
+function updatesStars(moves) {
+    rating = (moves > 60) ? 60 : moves;
+    rating = Math.floor(rating / 20);
+    console.log("ratings : " + rating + " , moves : " + moves);
+    var stars = "";
+    if (rating == 0) {
+        stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>";
+    } else if (rating == 1) {
+        stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li>";
+    } else if (rating == 2) {
+        stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>";
+    } else if (rating == 3) {
+        stars = "<li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>";
+    }
+    $(".stars").html(stars);
+}
 
- function updatesStars(moves){
-     rating = (moves > 60)?60:moves;
-     rating = Math.floor(rating/20);
-     console.log("ratings : "+rating+" , moves : "+moves);
-     var stars = "";
-     if(rating==0){
-         stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>";
-     }else if(rating == 1){
-         stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li>";
-     }else if(rating == 2){
-         stars = "<li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>";
-     }else if(rating == 3){
-         stars = "<li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>";
-     }
-     $(".stars").html(stars);
- }
+// the timer function
+// function updateTimer(status,reset) {
+//
+//     var sec = 0;
+//
+//     if(status){
+//         console.log("sec : "+sec);
+//          var id = setInterval(function () {
+//             sec++;
+//             $(".seconds").text(sec);
+//         }, 1000);
+//     }
+//     if(reset){
+//         clearInterval(id);
+//     }
+// }
 
- function updateTimer(status) {
+// start time function
+function startTimer() {
+    intervalId = setInterval(updateTime, 1000);
+}
 
-      sec = 0;
-     if(status){
-         console.log("sec : "+sec);
-         setInterval(function () {
-             sec++;
-             $(".seconds").text(sec);
-         },1000);
-     }
- }
+// update the seconds ellapsed in the dom
+function updateTime() {
 
- function checkIfAllMatched(){
-     var count_matched = 0;
+    $(".seconds").text(sec);
+    sec++;
+}
 
-     $(".card").each(function (){
+// this function clears the time for reset
+function resetTimer() {
+    clearInterval(intervalId);
+    sec = 0;
+}
+
+// check if all cards are matched
+function checkIfAllMatched() {
+    var count_matched = 0;
+
+    $(".card").each(function () {
         var check = $(this).attr('data-matched');
-       if(check=="true") {
-           count_matched++;
-       }
-     });
-     console.log("count_matched : "+count_matched);
-     if(count_matched == 2){
-         $('.popup').show();
-         var result_text = `with ${moves} moves and ${3 - rating} star in ${sec} sec`;
-         $('.result').text(result_text);
-     }
- }
+        if (check == "true") {
+            count_matched++;
+            console.log(count_matched);
+        }
+    });
+    console.log("count_matched : " + count_matched);
+    if (count_matched == 8) { //if cards are totally matched this calls the popup to show the result
+        $('.popup').addClass('open-popup');
+        var result_text = `with ${moves} moves and ${3 - rating} star(s) in ${--sec} seconds`;
+        $('.result').text(result_text);
+        resetTimer();
+    }
+}
